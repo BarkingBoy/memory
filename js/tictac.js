401607d41b8f8de4e7564a5c721e097581221d8a
Paul PERROT <paulperrot9@gmail.com>
2024-10-30 08:52:39 +0100

    Add reset function to tic tac toe

--- js/tictac.js
+++ js/tictac.js
@@ -1,17 +1,36 @@
-let currentPlayer = "X";
-const boardState = Array(9).fill(null);
+window.onload = init;
 
-const cells = document.querySelectorAll(".cell");
-cells.forEach((cell, index) => {
+function init() {
+  let currentPlayer = "X";
+  let boardState = Array(9).fill(null);
+  const playButton = document.querySelector(".play");
+  const cells = document.querySelectorAll(".cell");
+
+  cells.forEach((cell, index) => {
     cell.addEventListener("click", () => {
-        // Vérifie si la case est vide
-        if (!boardState[index]) {
-            // Marque la case pour le joueur actuel
-            boardState[index] = currentPlayer;
-            cell.textContent = currentPlayer;
+      // Vérifie si la case est vide
+      if (!boardState[index]) {
+        // Marque la case pour le joueur actuel
+        boardState[index] = currentPlayer;
+        cell.textContent = currentPlayer;
 
-            // Change de joueur
-            currentPlayer = currentPlayer === "X" ? "O" : "X";
-        }
+        // Change de joueur
+        currentPlayer = currentPlayer === "X" ? "O" : "X";
+      }
     });
-});
+  });
+
+  function reset() {
+    currentPlayer = "X";
+    boardState = Array(9).fill(null);
+
+    cells.forEach((cell) => {
+      cell.textContent = "";
+    });
+  }
+
+  playButton.addEventListener("click", () => {
+    console.log("button clicked");
+    reset();
+  });
+}
